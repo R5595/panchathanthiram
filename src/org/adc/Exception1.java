0ecee3939f34bc765e0a89242a11cb7b30116af6
R5595 <irudhayaraj001@gmail.com>
2021-09-20 08:08:29 +0530

    sucess

---
+++ src/org/adc/Exception1.java
@@ -0,0 +1,16 @@
+package org.adc;
+
+public class Exception1 {
+	
+	private void employeenotfoundexception() throws Throwable {
+		throw new Exception();
+		
+		
+		
+	}
+public static void main(String[] args) throws Throwable {
+	Exception1 e = new Exception1();
+	e.employeenotfoundexception();
+	
+}
+}
---
+++ src/org/adc/Sethu.java
@@ -0,0 +1,74 @@
+package org.adc;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Sethu {
+public static void main(String[] args) {
+int t=10;
+while (t>0) {
+	System.out.println(t);
+	t--;
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*	Set<Emp>rf=new HashSet<>();
+	
+	Emp e=new Emp ();
+	e.setId(1234);
+	e.setName("raj");
+	e.setPhone(918497865);
+	
+	Emp e1 =new Emp ();
+	e1.setId(2345);
+	e1.setName("thiu");
+e1.setPhone(12457617);
+
+rf.add(e);
+rf.add(e1);
+
+for (Emp list : rf) {
+	System.out.println(list.getId());
+	System.out.println(list.getName());
+	System.out.println(list.getPhone());
+}
+*/
+}
+	
+
+}
